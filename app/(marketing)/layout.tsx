23b06929eb4bcee8b43a7349332047e1cec49e98
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-30 22:20:30 -0700

    debugged user authentication and finished header component

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { Footer } from "./footer";
 import { Header } from "./header";
 
@@ -5,13 +6,15 @@ type Props = { children: React.ReactNode };
 
 const MarketingLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 flex flex-col items-center justify-center">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <ClerkProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 flex flex-col items-center justify-center">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </ClerkProvider>
   );
 };
 
