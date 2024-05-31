62337fb9daa38232e1f897354c3e63e58daa511a
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-30 22:37:07 -0700

    created user landing styling and functionality

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,60 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      This is a marketing page.
+    <div
+      className="mx-auto flex w-full max-w-[988px] flex-1 flex-col 
+    items-center justify-center gap-2 p-4 lg:flex-row"
+    >
+      <div
+        className="relative mb-8 h-[240px]
+       w-[240px] lg:mb-0 lg:h-[424px] lg:w-[424px]"
+      >
+        <Image src="/hero.svg" alt="Hero" fill loading="eager" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-center text-xl font-bold text-neutral-600 lg:text-3xl">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" fallbackRedirectUrl={"/"}>
+                <Button size={"lg"} variant={"secondary"} className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" fallbackRedirectUrl={"/learn"}>
+                <Button>I already have an account.</Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button
+                size={"lg"}
+                variant={"secondary"}
+                className="w-full"
+                asChild
+              >
+                <Link href={"/learn"}>Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
