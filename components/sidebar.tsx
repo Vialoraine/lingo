2440d08e7ea57eefd785b1c5843309b1a636779d
Loreina Salalila <vialoraine.gg@gmail.com>
2024-05-30 23:57:05 -0700

    finished styling navigation bars for both large and mobile displays

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,17 +1,71 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebarItem";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
 };
 
-export const SideBar = ({ className}: Props) => {
+const SideBarData = [
+  {
+    label: "Learn",
+    href: "/learn",
+    icon: "./learn.svg",
+  },
+  {
+    label: "Leaderboard",
+    href: "/leaderboard",
+    icon: "./leaderboard.svg",
+  },
+  {
+    label: "Quests",
+    href: "/quests",
+    icon: "./quests.svg",
+  },
+  {
+    label: "Shop",
+    href: "/shop",
+    icon: "./shop.svg",
+  },
+];
+
+export const SideBar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col", className,
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
       )}
     >
-      Sidebar
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/mascot.svg"} height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-1 flex-col gap-y-2">
+        {SideBarData.map((item) => (
+          <SidebarItem
+            key={item.href}
+            label={item.label}
+            href={item.href}
+            icon={item.icon}
+          />
+        ))}
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
