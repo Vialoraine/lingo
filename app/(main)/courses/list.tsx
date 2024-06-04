2d194ce914363e486c40cbc556a7dfe69ac0008a
Loreina Salalila <vialoraine.gg@gmail.com>
2024-06-04 00:08:45 -0700

    created a loading state

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { courses, userProgress } from "@/db/schema";
 import { Card } from "./card";
+import { useRouter } from "next/navigation";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
-  activeCourseId: number;
+  activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
 };
 
 export const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => (
--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,31 @@
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import {
+  challegeProgress,
+  courses,
+  lessons,
+  units,
+  userProgress,
+} from "./schema";
+import { eq } from "drizzle-orm";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+  return data;
+});
 
 export const getCourses = cache(async () => {
-  const data = await db.query.courses.findMany()
-  return data
-})
+  const data = await db.query.courses.findMany();
+  return data;
+});
