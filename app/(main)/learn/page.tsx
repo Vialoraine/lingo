0e828e5a0706eed8cd6d4d7127157c008ec3154e
Loreina Salalila <vialoraine.gg@gmail.com>
2024-06-03 22:58:14 -0700

    created and connected our serveless database using Neon and Drizzle

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -7,10 +7,18 @@ const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} />
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
       </StickyWrapper>
       <FeedWrapper>
         <Header title="Spanish" />
+        <div className="mb-10">
+          Test
+        </div>
       </FeedWrapper>
     </div>
   );
