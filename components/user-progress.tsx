b405be6246a68ef5fbe47e123faaefcd779f8311
Loreina Salalila <vialoraine.gg@gmail.com>
2024-06-04 01:21:24 -0700

    debugging user progress

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,26 +1,26 @@
 import Link from "next/link";
-import React from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: any;
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
-  hasActiveSubscription: boolean;
+  subscription: boolean;
 };
 
 export const UserProgress = ({
   activeCourse,
-  hasActiveSubscription,
   hearts,
   points,
+  subscription,
 }: Props) => {
   return (
-    <div className="flex w-full items-center justify-between gap-x-2">
-      <Link href="/courses">
-        <Button variant="ghost">
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href={"/courses"}>
+        <Button variant={"ghost"}>
           <Image
             src={activeCourse.imageSrc}
             alt={activeCourse.title}
@@ -30,28 +30,28 @@ export const UserProgress = ({
           />
         </Button>
       </Link>
-      <Link href="/shop">
-        <Button variant="ghost" className="text-orange-500">
+      <Link href={"/shop"}>
+        <Button variant={"ghost"} className="text-orange-500">
           <Image
-            src="/points.svg"
-            alt="Points"
+            src={"/points.svg"}
+            alt={"Points"}
+            className="mr-2"
             width={28}
             height={28}
-            className="mr-2"
           />
           {points}
         </Button>
       </Link>
-      <Link href="/shop">
-        <Button variant="ghost" className="text-rose-500">
+      <Link href={"/shop"}>
+        <Button variant={"ghost"} className="text-rose-500">
           <Image
-            src="/heart.svg"
-            alt="Hearts"
+            src={"/heart.svg"}
+            alt={"Hearts"}
+            className="mr-2"
             width={22}
             height={22}
-            className="mr-2"
           />
-          {hasActiveSubscription ? (
+          {subscription ? (
             <InfinityIcon className="h-4 w-4 stroke-[3]" />
           ) : (
             hearts
